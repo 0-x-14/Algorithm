9fe4d9270615d66a83f7ecfc987e161a90fd29fa
JooYeong Lee <0_x_14@naver.com>
2025-12-18 00:06:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int answer = 0;
+    while(N>=0) {
+      if(N%5 == 0) {
+        answer += N / 5;
+        System.out.println(answer);
+        return;
+      }
+      N -= 3;
+      answer++;
+    }
+    System.out.println(-1);
+  }
+}
